478802ad7df61c946be8bc0e2272276e62c12deb
Manish Kumar Sahu <msahu2595@gmail.com>
2022-11-12 00:30:55 +0530

    [INIT] Initialized project.

---
+++ App.js
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, Button, SafeAreaView} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '712761607011-pen5ucovsnc3pm7uf6hgic9k63s3bq6a.apps.googleusercontent.com',
+});
+
+function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  async function onGoogleButtonPress() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
+  function logout() {
+    return auth().signOut();
+  }
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <SafeAreaView>
+        <View>
+          <Text>Login</Text>
+          <Button
+            title="Google Sign-In"
+            onPress={() =>
+              onGoogleButtonPress().then(() =>
+                console.log('Signed in with Google!'),
+              )
+            }
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Welcome {user.email}</Text>
+        <Button
+          title="Sign Out"
+          onPress={() =>
+            logout().then(() => console.log('Signed out with Google!'))
+          }
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
