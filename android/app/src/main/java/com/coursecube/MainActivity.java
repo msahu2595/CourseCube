82a68c4fb5259702cfb63eaf6b745067f17509f0
Manish Kumar Sahu <msahu2595@gmail.com>
2022-11-20 20:17:13 +0530

    [ADD] react-navigation package.

--- android/app/src/main/java/com/coursecube/MainActivity.java
+++ android/app/src/main/java/com/coursecube/MainActivity.java
@@ -1,5 +1,7 @@
 package com.coursecube;
 
+import android.os.Bundle; // react-native-screens
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -23,6 +25,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+  // react-native-screens
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
