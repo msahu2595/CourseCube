f7b77fdab899ebde4b17b52d0f0aaafb338273f4
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-02-24 17:08:44 +0530

    added Add Test page

---
+++ apollo/mutations/ADD_TEST.js
@@ -0,0 +1,30 @@
+import {gql} from '@apollo/client';
+
+export const ADD_TEST = gql`
+  mutation AddTest($testInput: TestInput!) {
+    addTest(testInput: $testInput) {
+      payload {
+        _id
+        title
+        thumbnail
+        instructions
+        duration
+        totalMarks
+        negativeMark
+        enable
+        createdAt
+        updatedAt
+        questions {
+          _id
+          question
+          image
+          passage
+          options
+          mark
+          answerIndex
+          enable
+        }
+      }
+    }
+  }
+`;
