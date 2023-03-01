63af9496aae1108f8997a353e8ec687767583e0a
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-03-01 12:58:47 +0530

    Added changes to Video screen

--- screens/AdminScreens/AddVideosScreen.js
+++ screens/AdminScreens/AddVideosScreen.js
@@ -1,14 +1,18 @@
 import {ADD_VIDEOS} from '@mutations';
 import {useMutation} from '@apollo/client';
-import {Button, Text, TextInput, View} from 'react-native';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {SafeAreaContainer} from '@components';
-import React, {useState} from 'react';
+import React from 'react';
 import tw from 'twrnc';
 import {showMessage} from 'react-native-flash-message';
+import * as Yup from 'yup';
+import {Formik} from 'formik';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+const ValidationSchema = Yup.object().shape({
+  url: Yup.string().url('Invalid URL').required('URL is required'),
+});
 const AddVideosScreen = () => {
-  const [url, setUrl] = useState('');
-
   const [addVideo, {data, loading, error}] = useMutation(ADD_VIDEOS, {
     onCompleted: data => {
       console.log(' ==> addVideo', data);
@@ -25,25 +29,64 @@ const AddVideosScreen = () => {
   if (error) return <Text>`Submission error! ${error.message}`</Text>;
   return (
     <SafeAreaContainer>
-      <View style={tw`m-1 border p-1`}>
-        <Text style={tw`font-bold text-gray-900 text-lg`}>VIDEOS SCREEN</Text>
-        <Text style={tw`mx-2`}>Url :</Text>
-        <TextInput
-          placeholder="Enter Title"
-          onChangeText={setUrl}
-          value={url}
-          style={tw`border m-2 border-b-black`}
-        />
-        <Button
-          title="Submit"
-          onPress={() =>
+      <View style={tw`m-1 shadow p-1`}>
+        <Formik
+          initialValues={{
+            url: '',
+          }}
+          validationSchema={ValidationSchema}
+          onSubmit={values => {
+            console.log(values);
             addVideo({
               variables: {
-                videoLink: 'https://www.africau.edu/images/default/sample.pdf',
+                videoLink: values.url,
               },
-            })
-          }
-        />
+            });
+          }}>
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+          }) => (
+            <>
+              <Text style={tw`mx-2`}>Url :</Text>
+              <View
+                style={tw`mt-2 flex flex-row border border-black rounded-lg`}>
+                <MaterialCommunityIcons
+                  style={tw`border-r p-1 border-b-black`}
+                  name="link"
+                  color="#4F8EF7"
+                  size={25}
+                />
+                <TextInput
+                  placeholder="Enter Url"
+                  onChangeText={handleChange('url')}
+                  value={values.url}
+                  onBlur={handleBlur('url')}
+                  style={tw`h-10 w-80 p-2 font-popLight`}
+                />
+              </View>
+              {errors.url && touched.url && <Text>{errors.url}</Text>}
+              <View style={tw`m-3 shadow-sm`}>
+                <TouchableOpacity
+                  title="Submit"
+                  onPress={() => {
+                    console.log('submit');
+                    handleSubmit();
+                  }}
+                  style={tw`bg-blue-800 rounded-lg p-2`}>
+                  <Text
+                    style={tw`text-white text-center text-base font-popSemi`}>
+                    Submit
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </>
+          )}
+        </Formik>
       </View>
     </SafeAreaContainer>
   );
