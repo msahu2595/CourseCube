cced5f076e0a8764910343f1fc3e1e7c462ef550
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-03-03 16:58:51 +0530

    Added "headlines" query with fetchMore functionality #7

--- screens/AdminScreens/AdminHeadlineListScreen.js
+++ screens/AdminScreens/AdminHeadlineListScreen.js
@@ -1,8 +1,47 @@
-import React from 'react';
-import {Text} from 'react-native';
+import tw from '@lib/tailwind';
+import {HEADLINES} from '@queries';
+import {useQuery} from '@apollo/client';
+import React, {useCallback} from 'react';
+import {FlatList, Text, View} from 'react-native';
+import {NotificationItem, SafeAreaContainer} from '@components';
+
+const Separator = () => <View style={tw`h-2`} />;
 
 const AdminHeadlineListScreen = () => {
-  return <Text>AdminHeadlineListScreen</Text>;
+  const {loading, error, data, refetch, fetchMore} = useQuery(HEADLINES);
+
+  const renderItem = useCallback(
+    ({item, index}) => <NotificationItem index={index} {...item} />,
+    [],
+  );
+
+  if (error) return <Text>Error: {error.message}</Text>;
+
+  return (
+    <SafeAreaContainer>
+      <Text>AdminHeadlineListScreen</Text>
+      <FlatList
+        data={data?.headlines?.payload}
+        renderItem={renderItem}
+        keyExtractor={item => item._id}
+        ItemSeparatorComponent={Separator}
+        horizontal={false}
+        numColumns={2}
+        columnWrapperStyle={tw`justify-between`}
+        contentContainerStyle={tw`p-1`}
+        refreshing={loading}
+        onRefresh={refetch}
+        onEndReached={() =>
+          fetchMore({
+            variables: {
+              offset: data?.headlines?.payload.length,
+              limit: 10,
+            },
+          })
+        }
+      />
+    </SafeAreaContainer>
+  );
 };
 
 export default AdminHeadlineListScreen;
