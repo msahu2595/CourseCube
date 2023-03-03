2bec5f4b8727a8a578def6bfa331e3ec18b905e3
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-03-03 16:27:31 +0530

    navigation added to  AdminHeadlineListscreen

--- navigation_groups/myProfileStackGroup.js
+++ navigation_groups/myProfileStackGroup.js
@@ -11,6 +11,7 @@ import {
 import {TextInput, TouchableOpacity, View} from 'react-native';
 // Icon Import
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AdminHeadlineListScreen from 'screens/AdminScreens/AdminHeadlineListScreen';
 
 const Stack = createNativeStackNavigator(); // Create Stack Navigator
 
@@ -48,6 +49,11 @@ const myProfileStackGroup = () => {
         name="UserSearchScreen"
         component={UserSearchScreen}
         options={searchScreenOptions}
+      />
+      <Stack.Screen
+        name="AdminHeadlineListScreen"
+        component={AdminHeadlineListScreen}
+        options={{headerTitle: 'Admin Head line List'}}
       />
     </Stack.Group>
   );
