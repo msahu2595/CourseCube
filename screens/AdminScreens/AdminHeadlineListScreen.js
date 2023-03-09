5e2c19fd3a8ee6af4b306f73ac330f44ff82a6ba
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-03-09 19:26:33 +0530

    showing successful message using "showMessage"  #8

--- screens/AdminScreens/AdminHeadlineListScreen.js
+++ screens/AdminScreens/AdminHeadlineListScreen.js
@@ -1,11 +1,13 @@
+import * as Yup from 'yup';
 import tw from '@lib/tailwind';
 import {HEADLINES} from '@queries';
-import {useQuery} from '@apollo/client';
+import {CREATE_HEADLINE} from '@mutations';
+import {useMutation, useQuery} from '@apollo/client';
 import React, {useCallback, useRef, useState} from 'react';
 import {NotificationItem, SafeAreaContainer} from '@components';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {
-  Alert,
+  ActivityIndicator,
   FlatList,
   Modal,
   Pressable,
@@ -14,9 +16,9 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import {Formik} from 'formik';
+import {showMessage} from 'react-native-flash-message';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-// import {ADD_HEADLINE} from '@mutations';
-import AddHeadlineScreen from './AddHeadlineScreen';
 
 const Separator = () => <View style={tw`h-2`} />;
 
@@ -61,29 +63,6 @@ const AdminHeadlineListScreen = () => {
           </TouchableOpacity>
         </View>
         <View>
-          <Modal
-            animationType="slide"
-            transparent={false}
-            visible={modalVisible}
-            onRequestClose={() => {
-              Alert.alert('Modal has been closed');
-              setModalVisible(!modalVisible);
-            }}>
-            <View>
-              <Text>{AddHeadlineScreen}</Text>
-              <TextInput
-                style={tw`border`}
-                placeholder="CreateHeadline"
-                onChangeText={Text}
-                value={text}
-              />
-              <Pressable onPress={() => setModalVisible(!modalVisible)}>
-                <View style={tw` p-6 items-center `}>
-                  <Text style={tw`border p-2  `}>Hide Modal</Text>
-                </View>
-              </Pressable>
-            </View>
-          </Modal>
           <Pressable onPress={() => setModalVisible(true)}>
             <MaterialCommunityIcons
               name="plus"
@@ -113,8 +92,177 @@ const AdminHeadlineListScreen = () => {
           })
         }
       />
+      <AddHeadlineModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+      />
     </SafeAreaContainer>
   );
 };
 
 export default AdminHeadlineListScreen;
+
+const ValidationSchema = Yup.object().shape({
+  description: Yup.string().required('description is required'),
+  image: Yup.string().url('invalid image URL'),
+  link: Yup.string().url('invalid link'),
+});
+
+const AddHeadlineModal = ({visible, onClose}) => {
+  const [createHeadline, {loading: mutationLoading}] = useMutation(
+    CREATE_HEADLINE,
+    {
+      onCompleted: data => {
+        console.log('onCompleted', data);
+        showMessage({
+          message: 'Haedline Added Successfully.',
+          type: 'success',
+        });
+      },
+      onError: error => {
+        console.log('onError', error.message);
+        showMessage({
+          message: 'Error.',
+          type: 'error',
+        });
+      },
+    },
+  );
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}>
+      <View style={tw`flex-1 items-center justify-center`}>
+        <View style={tw`bg-white`}>
+          <View style={tw`m-1 shadow p-1`}>
+            <Formik
+              initialValues={{
+                description: '',
+                image: '',
+                link: '',
+              }}
+              validationSchema={ValidationSchema}
+              onSubmit={values => {
+                console.log(values);
+                const headlineInput = {description: values.description};
+                if (values.image) {
+                  headlineInput.image = values.image;
+                }
+                if (values.link) {
+                  headlineInput.link = values.link;
+                }
+                createHeadline({variables: {headlineInput}});
+              }}>
+              {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                values,
+                errors,
+                touched,
+              }) => (
+                <>
+                  <View style={tw`flex-row`}>
+                    <Text
+                      style={tw`flex-1 text-center font-popBold text-lg m-4`}>
+                      Create Headline
+                    </Text>
+                    <TouchableOpacity onPress={onClose}>
+                      <MaterialIcons
+                        name="clear"
+                        size={25}
+                        style={tw`p-1 items-center`}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                  <Text style={tw`mx-2`}>Description (required): </Text>
+                  <View
+                    style={tw`mt-2 flex flex-row border border-black rounded-lg`}>
+                    <MaterialCommunityIcons
+                      style={tw`border-r p-1 border-b-black`}
+                      name="link"
+                      color="#4F8EF7"
+                      size={25}
+                    />
+                    <TextInput
+                      editable={!mutationLoading}
+                      placeholder="Enter description"
+                      onChangeText={handleChange('description')}
+                      value={values.description}
+                      onBlur={handleBlur('description')}
+                      style={tw`h-10 w-80 p-2 font-popLight`}
+                    />
+                  </View>
+                  {errors.description && touched.description && (
+                    <Text>{errors.description}</Text>
+                  )}
+                  <View
+                    style={tw`mt-2 flex flex-row border border-black rounded-lg`}>
+                    <MaterialCommunityIcons
+                      style={tw`border-r p-1 border-b-black`}
+                      name="link"
+                      color="#4F8EF7"
+                      size={25}
+                    />
+                    <TextInput
+                      editable={!mutationLoading}
+                      placeholder="Enter image link"
+                      onChangeText={handleChange('image')}
+                      value={values.image}
+                      onBlur={handleBlur('image')}
+                      style={tw`h-10 w-80 p-2 font-popLight`}
+                    />
+                  </View>
+                  {errors.image && touched.image && <Text>{errors.image}</Text>}
+                  <View
+                    style={tw`mt-2 flex flex-row border border-black rounded-lg`}>
+                    <MaterialCommunityIcons
+                      style={tw`border-r p-1 border-b-black`}
+                      name="link"
+                      color="#4F8EF7"
+                      size={25}
+                    />
+                    <TextInput
+                      editable={!mutationLoading}
+                      placeholder="Enter link"
+                      onChangeText={handleChange('link')}
+                      value={values.link}
+                      onBlur={handleBlur('link')}
+                      style={tw`h-10 w-80 p-2 font-popLight`}
+                    />
+                  </View>
+                  {errors.link && touched.link && <Text>{errors.link}</Text>}
+                  <View style={tw`m-3 shadow-sm`}>
+                    <TouchableOpacity
+                      disabled={mutationLoading}
+                      title="Submit"
+                      onPress={() => {
+                        console.log(values);
+                        handleSubmit();
+                      }}
+                      style={tw`bg-blue-800 rounded-lg p-2`}>
+                      <Text
+                        style={tw`text-white text-center text-base font-popSemi`}>
+                        Submit
+                      </Text>
+                      {mutationLoading && (
+                        <ActivityIndicator animating={true} size="small" />
+                      )}
+                    </TouchableOpacity>
+                  </View>
+                </>
+              )}
+            </Formik>
+          </View>
+          <Pressable onPress={onClose}>
+            <View style={tw` p-6 items-center `}>
+              <Text style={tw`border p-2  `}>Hide Modal</Text>
+            </View>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+};
