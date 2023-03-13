0be8e2035c2f3ab65d2c4935b329c8fbb9942eb1
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-03-13 13:34:15 +0530

    edit advert mutation added #10 #12

--- screens/AdminScreens/AdminAdvertListScreen.js
+++ screens/AdminScreens/AdminAdvertListScreen.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 import tw from '@lib/tailwind';
 import {Formik} from 'formik';
 import {ADVERTS} from '@queries';
-import {CREATE_ADVERT} from '@mutations';
+import {CREATE_ADVERT, EDIT_ADVERT} from '@mutations';
 import React, {useCallback, useState} from 'react';
 import {useMutation, useQuery} from '@apollo/client';
 import {showMessage} from 'react-native-flash-message';
@@ -10,6 +10,7 @@ import {AdvertItem, SafeAreaContainer} from '@components';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {
   ActivityIndicator,
+  Button,
   FlatList,
   Modal,
   Pressable,
@@ -24,20 +25,34 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 const Separator = () => <View style={tw`h-2`} />;
 
 const AdminAdvertListScreen = () => {
-  const [modalVisible, setModalVisible] = useState(false);
+  const [createModalVisible, setCreateModalVisible] = useState(false);
   const [isEnabled, setIsEnabled] = useState(false);
-  const {loading, error, data, refetch, fetchMore} = useQuery(ADVERTS, {
-    variables: {
-      filter: {
-        type: 'TINY',
-      },
-    },
-  });
-
-  const renderItem = useCallback(
-    ({item, index}) => <AdvertItem index={index} {...item} />,
-    [],
+  const [editAdvertModal, setEditAdvertModal] = useState(null);
+  const {loading, error, data, refetch, fetchMore} = useQuery(
+    ADVERTS,
+    //    {
+    //   variables: {
+    //     filter: {
+    //       type: '',
+    //     },
+    //   },
+    // }
   );
+
+  const renderItem = useCallback(({item, index}) => {
+    return (
+      <View index={index} {...item}>
+        <AdvertItem index={index} {...item} />
+        <View style={tw`flex flex-row justify-evenly`}>
+          <Button
+            onPress={() => setEditAdvertModal(item)}
+            title="Edit"
+            color="#841584"
+          />
+        </View>
+      </View>
+    );
+  }, []);
 
   if (error) return <Text>Error: {error.message}</Text>;
 
@@ -87,7 +102,7 @@ const AdminAdvertListScreen = () => {
         />
         <View style={tw`bg-slate-500`}>
           <View>
-            <Pressable onPress={() => setModalVisible(true)}>
+            <Pressable onPress={() => setCreateModalVisible(true)}>
               <MaterialCommunityIcons
                 name="plus"
                 size={30}
@@ -117,9 +132,13 @@ const AdminAdvertListScreen = () => {
           })
         }
       />
-      <AddAdvertModal
-        visible={modalVisible}
-        onClose={() => setModalVisible(false)}
+      <CreateAdvertModal
+        visible={createModalVisible}
+        onClose={() => setCreateModalVisible(false)}
+      />
+      <EditAdvertModal
+        visible={editAdvertModal}
+        onClose={() => setEditAdvertModal(null)}
       />
     </SafeAreaContainer>
   );
@@ -133,7 +152,7 @@ const ValidationSchema = Yup.object().shape({
   type: Yup.string(),
 });
 
-const AddAdvertModal = ({visible, onClose}) => {
+const CreateAdvertModal = ({visible, onClose}) => {
   const [createAdvert, {loading: mutationLoading}] = useMutation(
     CREATE_ADVERT,
     {
@@ -197,6 +216,176 @@ const AddAdvertModal = ({visible, onClose}) => {
                     <Text
                       style={tw`flex-1 text-center font-popBold text-lg m-4`}>
                       Create Advertisement
+                    </Text>
+                    <TouchableOpacity onPress={onClose}>
+                      <MaterialIcons
+                        name="clear"
+                        size={25}
+                        style={tw`p-1 items-center`}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                  <Text style={tw`mx-2`}>Type (required!) : </Text>
+                  <View
+                    style={tw`mt-2 flex-row border border-black rounded-lg`}>
+                    <FlatList
+                      horizontal
+                      data={['TINY', 'SMALL', 'MEDIUM', 'LARGE']}
+                      renderItem={({item}) => (
+                        <TouchableOpacity
+                          style={{
+                            backgroundColor:
+                              values.type === item
+                                ? tw.color('blue-600')
+                                : 'white',
+                          }}
+                          onPress={() => setFieldValue('type', item)}>
+                          <Text>{item}</Text>
+                        </TouchableOpacity>
+                      )}
+                      keyExtractor={item => item}
+                      style={tw`bg-white`}
+                      contentContainerStyle={tw`py-2`}
+                      showsHorizontalScrollIndicator={false}
+                      ItemSeparatorComponent={() => <View style={tw`w-8`} />}
+                      ListHeaderComponent={() => <View style={tw`w-8`} />}
+                      ListFooterComponent={() => <View style={tw`w-2`} />}
+                    />
+                  </View>
+                  {errors.type && touched.type && <Text>{errors.type}</Text>}
+                  <Text style={tw`pt-4 mx-2`}>Image Link (required!) :</Text>
+                  <View
+                    style={tw`mt-2 flex flex-row border border-black rounded-lg`}>
+                    <MaterialCommunityIcons
+                      style={tw`border-r p-1 border-b-black`}
+                      name="link"
+                      color="#4F8EF7"
+                      size={25}
+                    />
+                    <TextInput
+                      editable={!mutationLoading}
+                      placeholder="Enter image link"
+                      onChangeText={handleChange('image')}
+                      value={values.image}
+                      onBlur={handleBlur('image')}
+                      style={tw`h-10 w-80 p-2 bg-white font-popLight`}
+                    />
+                  </View>
+                  {errors.image && touched.image && <Text>{errors.image}</Text>}
+                  <Text style={tw`pt-4 mx-2`}>Reference Link:</Text>
+                  <View
+                    style={tw`mt-2 flex flex-row border border-black rounded-lg`}>
+                    <MaterialCommunityIcons
+                      style={tw`border-r p-1 border-b-black`}
+                      name="link"
+                      color="#4F8EF7"
+                      size={25}
+                    />
+                    <TextInput
+                      editable={!mutationLoading}
+                      placeholder="Enter Reference link"
+                      onChangeText={handleChange('link')}
+                      value={values.link}
+                      onBlur={handleBlur('link')}
+                      style={tw`h-10 w-80 p-2 bg-white font-popLight`}
+                    />
+                  </View>
+                  {errors.link && touched.link && <Text>{errors.link}</Text>}
+                  <View style={tw`m-3 shadow-sm pt-4`}>
+                    <TouchableOpacity
+                      disabled={mutationLoading}
+                      title="Submit"
+                      onPress={() => {
+                        console.log(values);
+                        handleSubmit();
+                      }}
+                      style={tw`bg-blue-800 rounded-lg p-2`}>
+                      <Text
+                        style={tw`text-white text-center text-base font-popSemi`}>
+                        Submit
+                      </Text>
+                      {mutationLoading && (
+                        <ActivityIndicator animating={true} size="small" />
+                      )}
+                    </TouchableOpacity>
+                  </View>
+                </>
+              )}
+            </Formik>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const EditAdvertValidationSchema = Yup.object().shape({
+  image: Yup.string().url('invalid image URL'),
+  link: Yup.string().url('invalid link'),
+  type: Yup.string(),
+});
+
+const EditAdvertModal = ({visible, onClose}) => {
+  const [editAdvert, {loading: mutationLoading}] = useMutation(EDIT_ADVERT, {
+    onCompleted: data => {
+      console.log('onCompleted', data);
+      onClose();
+      showMessage({
+        message: 'Haedline Added Successfully.',
+        type: 'success',
+      });
+    },
+    onError: error => {
+      console.log('onError', error.message);
+      showMessage({
+        message: 'Error.',
+        type: 'error',
+      });
+    },
+  });
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}>
+      <View style={tw`flex-1 items-center justify-center`}>
+        <View style={tw`bg-slate-100`}>
+          <View style={tw`m-1 shadow p-1`}>
+            <Formik
+              initialValues={{
+                image: '',
+                link: '',
+                type: '',
+              }}
+              validationSchema={EditAdvertValidationSchema}
+              onSubmit={values => {
+                console.log(values);
+                editAdvert({
+                  variables: {
+                    advertInput: {
+                      image: values.image,
+                      type: values.type,
+                      link: values.link,
+                    },
+                  },
+                });
+              }}>
+              {({
+                handleChange,
+                setFieldValue,
+                handleBlur,
+                handleSubmit,
+                values,
+                errors,
+                touched,
+              }) => (
+                <>
+                  <View style={tw`flex-row`}>
+                    <Text
+                      style={tw`flex-1 text-center font-popBold text-lg m-4`}>
+                      Edit Advertisement
                     </Text>
                     <TouchableOpacity onPress={onClose}>
                       <MaterialIcons
