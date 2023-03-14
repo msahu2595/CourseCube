137718337f4930c481870d571c19b2367255ae73
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-03-14 17:02:59 +0530

    EditBundle Mutation added #13

---
+++ apollo/mutations/EDIT_BUNDLE_CONTENT.js
@@ -0,0 +1,72 @@
+import {gql} from '@apollo/client';
+
+export const EDIT_BUNDLE_CONTENT = gql`
+  mutation EditBundleContent(
+    $bundleContentId: ID!
+    $bundleContentInput: BundleContentInput!
+  ) {
+    editBundleContent(
+      bundleContentId: $bundleContentId
+      bundleContentInput: $bundleContentInput
+    ) {
+      payload {
+        _id
+        subjectId
+        subject
+        image
+        title
+        media {
+          ... on Video {
+            _id
+            title
+            thumbnail
+            time
+            link
+            enable
+            createdAt
+            updatedAt
+          }
+          ... on Test {
+            _id
+            title
+            thumbnail
+            instructions
+            duration
+            questions {
+              _id
+              question
+              image
+              passage
+              options
+              answerIndex
+              mark
+              negativeMark
+            }
+            totalMarks
+            enable
+            createdAt
+            updatedAt
+          }
+          ... on Document {
+            _id
+            title
+            thumbnail
+            url
+            pages
+            enable
+            createdAt
+            updatedAt
+          }
+        }
+        type
+        language
+        description
+        visible
+        enable
+        createdAt
+        updatedAt
+        purchased
+      }
+    }
+  }
+`;
---
+++ components/EditBundleContentModal.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const EditBundleContentModal = () => {
+  return <View>EditBundleContentModal</View>;
+};
+
+export default EditBundleContentModal;
