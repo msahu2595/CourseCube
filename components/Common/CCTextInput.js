9046e9abb73c1e2360ba2194cf2dbc1371bfa527
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-03-17 16:53:10 +0530

    syntax error solved

--- components/Common/CCTextInput.js
+++ components/Common/CCTextInput.js
@@ -3,23 +3,21 @@ import React, {memo} from 'react';
 import {Text, TextInput, View} from 'react-native';
 
 export const CCTextInput = memo(
-  ({label, required = false, errors, touched, ...rest}) => (
+  ({label, required = false, error, touched, ...rest}) => (
     <View style={tw`mb-1`}>
       <Text style={tw`text-sm text-gray-600 font-avReg p-1`}>
         {label}
-        {/* {required && <Text style={tw`text-red-600`}>*</Text>} */}
+        {required && <Text style={tw`text-red-600`}>*</Text>}
       </Text>
       <TextInput
         style={tw.style('border border-gray-300 rounded-lg px-2', {
-          textAlignVertical: 'top',
+          textAlignVertical: rest?.multiline ? 'top' : 'center',
         })}
         {...rest}
       />
-      {/* {errors.title && touched.title ? (
-        <Text style={tw`text-sm text-red-600 font-avReg p-1`}>
-          {errors.title}
-        </Text>
-      ) : null} */}
+      {error && touched ? (
+        <Text style={tw`text-sm text-red-600 font-avReg p-1`}>{error}</Text>
+      ) : null}
     </View>
   ),
 );
