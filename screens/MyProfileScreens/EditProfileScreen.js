dbaf34482c7645ded6c166af1c1d8a36246f74da
Manish Kumar Sahu <msahu2595@gmail.com>
2023-06-03 19:15:29 +0530

    [ADD] Info & address field into Edit & Create profile.

--- screens/MyProfileScreens/EditProfileScreen.js
+++ screens/MyProfileScreens/EditProfileScreen.js
@@ -68,7 +68,21 @@ const EditProfileValidationSchema = yup.object({
     .string()
     .required('Please write something about yourself.')
     .min(7, 'Too short!!')
-    .max(320, 'Too big!!'),
+    .max(320, 'Too long!!'),
+  education: yup.string().max(80, 'Too long!!'),
+  workAt: yup.string().max(80, 'Too long!!'),
+  workAs: yup.string().max(80, 'Too long!!'),
+  pincode: yup
+    .string()
+    .matches(/^[1-9]{1}[0-9]{5}$/, 'Please enter valid pincode.')
+    .max(80, 'Too long!!'),
+  country: yup.string().max(80, 'Too long!!'),
+  state: yup.string().max(80, 'Too long!!'),
+  district: yup.string().max(80, 'Too long!!'),
+  cityVillage: yup.string().max(80, 'Too long!!'),
+  area: yup.string().max(80, 'Too long!!'),
+  street: yup.string().max(80, 'Too long!!'),
+  landmark: yup.string().max(80, 'Too long!!'),
 });
 
 const EditProfileScreen = () => {
@@ -113,15 +127,57 @@ const EditProfileScreen = () => {
           initialValues={{
             picture: loggedUser?.picture ?? '',
             about: loggedUser?.about ?? '',
+            education: loggedUser?.education ?? '',
+            workAt: loggedUser?.workAt ?? '',
+            workAs: loggedUser?.workAs ?? '',
+            pincode: loggedUser?.pincode ?? '',
+            country: loggedUser?.country ?? '',
+            state: loggedUser?.state ?? '',
+            district: loggedUser?.district ?? '',
+            cityVillage: loggedUser?.cityVillage ?? '',
+            area: loggedUser?.area ?? '',
+            street: loggedUser?.street ?? '',
+            landmark: loggedUser?.landmark ?? '',
           }}
           validationSchema={EditProfileValidationSchema}
           onSubmit={values => {
             console.log({values});
-            const userInput = {
-              about: values.about,
-            };
+            const userInput = {about: values.about};
             if (values.picture) {
               userInput.picture = values.picture;
+            }
+            if (values.education) {
+              userInput.education = values.education;
+            }
+            if (values.workAt) {
+              userInput.workAt = values.workAt;
+            }
+            if (values.workAs) {
+              userInput.workAs = values.workAs;
+            }
+            if (values.pincode) {
+              userInput.pincode = values.pincode;
+            }
+            if (values.country) {
+              userInput.country = values.country;
+            }
+            if (values.state) {
+              userInput.state = values.state;
+            }
+            if (values.district) {
+              userInput.district = values.district;
+            }
+            if (values.cityVillage) {
+              userInput.cityVillage = values.cityVillage;
+            }
+            if (values.area) {
+              userInput.area = values.area;
+            }
+            if (values.street) {
+              userInput.street = values.street;
+            }
+            if (values.landmark) {
+              userInput.landmark = values.landmark;
             }
             editProfile({variables: {userInput}});
           }}>
@@ -135,11 +191,11 @@ const EditProfileScreen = () => {
             touched,
           }) => (
             <>
-              <View style={tw`py-2`}>
+              <View style={tw`pt-2 pb-4`}>
                 <View style={tw`items-center justify-center py-2`}>
                   <CCImageUploader
                     name={loggedUser._id}
-                    gender={values?.gender}
+                    gender={loggedUser.gender}
                     value={values?.picture}
                     onUploadImage={value => {
                       setFieldValue('picture', value);
@@ -196,6 +252,111 @@ const EditProfileScreen = () => {
                     }}
                   />
                 )}
+                <CCTextInput
+                  label="Education"
+                  error={errors.education}
+                  touched={touched.education}
+                  onChangeText={handleChange('education')}
+                  onBlur={handleBlur('education')}
+                  value={values.education}
+                  editable={!submitting}
+                />
+                <CCTextInput
+                  label="Work At"
+                  error={errors.workAt}
+                  touched={touched.workAt}
+                  onChangeText={handleChange('workAt')}
+                  onBlur={handleBlur('workAt')}
+                  value={values.workAt}
+                  editable={!submitting}
+                />
+                <CCTextInput
+                  label="Work As"
+                  error={errors.workAs}
+                  touched={touched.workAs}
+                  onChangeText={handleChange('workAs')}
+                  onBlur={handleBlur('workAs')}
+                  value={values.workAs}
+                  editable={!submitting}
+                />
+                <CCTextInput
+                  label="Pincode"
+                  error={errors.pincode}
+                  touched={touched.pincode}
+                  onChangeText={value => {
+                    console.log(value);
+                    setFieldValue('pincode', value);
+                    if (value?.length >= 6) {
+                      fetch(`https://api.postalpincode.in/pincode/${value}`, {
+                        method: 'get',
+                      })
+                        .then(response => response.json())
+                        .then(response => {
+                          const data = response[0]?.PostOffice[0];
+                          setFieldValue('country', data?.Country);
+                          setFieldValue('state', data?.State);
+                          setFieldValue('district', data?.District);
+                        })
+                        .catch(err => {
+                          console.log(err?.message);
+                        });
+                    }
+                  }}
+                  onBlur={handleBlur('pincode')}
+                  value={values.pincode}
+                  editable={!submitting}
+                />
+                <CCTextInput
+                  label="Country"
+                  value={values.country}
+                  editable={false}
+                />
+                <CCTextInput
+                  label="State"
+                  value={values.state}
+                  editable={false}
+                />
+                <CCTextInput
+                  label="District"
+                  value={values.district}
+                  editable={false}
+                />
+                <CCTextInput
+                  label="City/Village"
+                  error={errors.cityVillage}
+                  touched={touched.cityVillage}
+                  onChangeText={handleChange('cityVillage')}
+                  onBlur={handleBlur('cityVillage')}
+                  value={values.cityVillage}
+                  editable={!submitting}
+                />
+                <CCTextInput
+                  label="Area"
+                  error={errors.area}
+                  touched={touched.area}
+                  onChangeText={handleChange('area')}
+                  onBlur={handleBlur('area')}
+                  value={values.area}
+                  editable={!submitting}
+                />
+                <CCTextInput
+                  label="Street"
+                  error={errors.street}
+                  touched={touched.street}
+                  onChangeText={handleChange('street')}
+                  onBlur={handleBlur('street')}
+                  value={values.street}
+                  editable={!submitting}
+                />
+                <CCTextInput
+                  label="Landmark"
+                  error={errors.landmark}
+                  touched={touched.landmark}
+                  onChangeText={handleChange('landmark')}
+                  onBlur={handleBlur('landmark')}
+                  value={values.landmark}
+                  editable={!submitting}
+                />
               </View>
               <CCButton
                 label="Submit"
