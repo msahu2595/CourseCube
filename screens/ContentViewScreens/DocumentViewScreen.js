7b9d951bb1ef1cb15087fcd02d974b8fd567119c
Manish Kumar Sahu <msahu2595@gmail.com>
2023-06-04 11:26:26 +0530

    [ADD] Like & Bookmark button on document view screen.

--- screens/ContentViewScreens/DocumentViewScreen.js
+++ screens/ContentViewScreens/DocumentViewScreen.js
@@ -1,11 +1,14 @@
 import tw from '@lib/tailwind';
 import {CONTENT} from '@queries';
 import Pdf from 'react-native-pdf';
-import React, {useCallback} from 'react';
 import openWebURL from 'utils/openWebURL';
 import {SafeAreaContainer} from '@components';
+import React, {useCallback, useEffect} from 'react';
 import {showMessage} from 'react-native-flash-message';
 import {gql, useMutation, useQuery} from '@apollo/client';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {CCBookmarkButton, CCLikeButton} from 'components/Common';
 import {View, Alert, ActivityIndicator, Text} from 'react-native';
 
 const ADD_VIEW = gql`
@@ -19,7 +22,7 @@ const ADD_VIEW = gql`
   }
 `;
 
-const DocumentViewScreen = ({route}) => {
+const DocumentViewScreen = ({navigation, route}) => {
   const {
     loading: queryLoading,
     data: queryData,
@@ -27,6 +30,50 @@ const DocumentViewScreen = ({route}) => {
   } = useQuery(CONTENT, {
     variables: {contentId: route.params.contentId},
   });
+
+  useEffect(() => {
+    const payload = queryData?.content?.payload;
+    if (payload) {
+      navigation.setOptions({
+        headerRight: () => (
+          <>
+            <CCLikeButton
+              refId={payload?._id}
+              initial={payload?.liked === 1 ? true : false}
+              refetchQueries={[
+                {query: CONTENT, variables: {contentId: payload?._id}},
+              ]}>
+              {liked => (
+                <AntDesign
+                  size={20}
+                  color={tw.color('black')}
+                  name={liked ? 'like1' : 'like2'}
+                  style={tw`py-2 px-3`}
+                />
+              )}
+            </CCLikeButton>
+            <CCBookmarkButton
+              refId={payload?._id}
+              type={payload?.__typename}
+              subType={payload?.media?.__typename}
+              initial={payload?.bookmarked === 1 ? true : false}
+              refetchQueries={[
+                {query: CONTENT, variables: {contentId: payload?._id}},
+              ]}>
+              {bookmarked => (
+                <FontAwesome
+                  size={20}
+                  color={tw.color('black')}
+                  name={bookmarked ? 'bookmark' : 'bookmark-o'}
+                  style={tw`py-2 px-3`}
+                />
+              )}
+            </CCBookmarkButton>
+          </>
+        ),
+      });
+    }
+  }, [navigation, queryData]);
 
   const data = queryData?.content?.payload || {};
 
