c4274b6e7c3413bae58e1a11400a86ed5ec297c9
Manish Kumar Sahu <msahu2595@gmail.com>
2023-12-08 12:03:55 +0530

    [REMOVE] Whatsapp login.

--- screens/AuthScreens/LoginIntroScreen.js
+++ screens/AuthScreens/LoginIntroScreen.js
@@ -1,32 +1,31 @@
-import React, {memo, useEffect} from 'react';
 import {
   Text,
   View,
   Image,
-  Linking,
   Platform,
   StatusBar,
   ImageBackground,
   TouchableOpacity,
 } from 'react-native';
+import tw from '@lib/tailwind';
+import React, {memo} from 'react';
+import {GOOGLE_LOG_IN} from '@mutations';
+import {useMutation} from '@apollo/client';
+import {LoadingIndicator} from '@components';
 import {
   statusCodes,
   GoogleSignin,
 } from '@react-native-google-signin/google-signin';
-import {LoadingIndicator, SafeAreaContainer} from '@components';
-import {GOOGLE_LOG_IN, WHATSAPP_LOG_IN} from '@mutations';
-import messaging from '@react-native-firebase/messaging';
-import {showMessage} from 'react-native-flash-message';
 import {loggedUserVar, storage} from 'apollo/client';
-import {useMutation} from '@apollo/client';
-import tw from '@lib/tailwind';
+import {showMessage} from 'react-native-flash-message';
+import messaging from '@react-native-firebase/messaging';
 
 GoogleSignin.configure({
   webClientId:
     '712761607011-pen5ucovsnc3pm7uf6hgic9k63s3bq6a.apps.googleusercontent.com',
 });
 
-const LoginIntroScreen = ({route}) => {
+const LoginIntroScreen = () => {
   const [googleLogIn, {loading: googleLogInLoading}] = useMutation(
     GOOGLE_LOG_IN,
     {
@@ -43,49 +42,6 @@ const LoginIntroScreen = ({route}) => {
       },
     },
   );
-
-  const [whatsAppLogIn, {loading: whatsAppLogInLoading}] = useMutation(
-    WHATSAPP_LOG_IN,
-    {
-      onCompleted: data => {
-        storage.set('user', JSON.stringify(data?.whatsAppLogIn?.payload));
-        loggedUserVar(data?.whatsAppLogIn?.payload);
-      },
-      onError: err => {
-        showMessage({
-          message: err?.message || 'Some unknown error occurred. Try again!!',
-          type: 'danger',
-          icon: 'danger',
-        });
-      },
-    },
-  );
-
-  useEffect(() => {
-    async function login(waId) {
-      try {
-        const FCMToken =
-          Platform.OS === 'ios' ? '' : await messaging().getToken();
-        whatsAppLogIn({
-          variables: {
-            waId,
-            FCMToken,
-            platform: Platform.OS,
-            acceptTnC: true,
-          },
-        });
-      } catch (error) {
-        showMessage({
-          message: error?.message || 'Some unknown error occurred. Try again!!',
-          type: 'danger',
-          icon: 'danger',
-        });
-      }
-    }
-    if (route.params?.waId) {
-      login(route.params?.waId);
-    }
-  }, [route.params?.waId, whatsAppLogIn]);
 
   const handleGoogleSignIn = async () => {
     try {
@@ -119,28 +75,6 @@ const LoginIntroScreen = ({route}) => {
     }
   };
 
-  const handleWhatsAppSignIn = async () => {
-    try {
-      const url =
-        'https://courseqube.authlink.me?redirectUri=coursequbeotpless://otpless';
-      if (await Linking.canOpenURL(url)) {
-        await Linking.openURL(url);
-      } else {
-        showMessage({
-          message: 'Facing issue with whatsapp login, please try again.',
-          type: 'danger',
-          icon: 'danger',
-        });
-      }
-    } catch (error) {
-      showMessage({
-        message: error?.message || 'Some unknown error occurred. Try again!!',
-        type: 'danger',
-        icon: 'danger',
-      });
-    }
-  };
-
   return (
     <>
       <StatusBar backgroundColor="#185366" />
@@ -162,18 +96,11 @@ const LoginIntroScreen = ({route}) => {
             onPress={handleGoogleSignIn}
             imageStyle={tw`h-8 w-8`}
             imageSource={require('@images/google-48.png')}
-            disabled={googleLogInLoading || whatsAppLogInLoading}
-          />
-          <View style={tw`w-2`} />
-          <LoginButton
-            onPress={handleWhatsAppSignIn}
-            imageStyle={tw`h-10 w-10`}
-            imageSource={require('@images/whatsapp-52.png')}
-            disabled={googleLogInLoading || whatsAppLogInLoading}
+            disabled={googleLogInLoading}
           />
         </View>
       </ImageBackground>
-      <LoadingIndicator loading={googleLogInLoading || whatsAppLogInLoading} />
+      <LoadingIndicator loading={googleLogInLoading} />
     </>
   );
 };
