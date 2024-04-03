85b2d6dea814caae4215a68e7adbd5f8fced7a82
Manish Kumar Sahu <msahu2595@gmail.com>
2024-04-03 13:37:23 +0530

    [ADD] User list screen in Admin panel.

--- components/FollowItem.js
+++ components/FollowItem.js
@@ -1,8 +1,10 @@
 import tw from '@lib/tailwind';
 import {useGenderImage} from 'hooks';
 import React, {useCallback} from 'react';
+import config from 'react-native-ultimate-config';
 import {useNavigation} from '@react-navigation/core';
 import {Image, Text, View, Pressable} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const FollowItem = ({
   _id,
@@ -11,6 +13,8 @@ const FollowItem = ({
   picture,
   followers,
   activities,
+  isAdmin = false,
+  ...rest
 }) => {
   const navigation = useNavigation();
 
@@ -25,7 +29,15 @@ const FollowItem = ({
       onPress={handleNavigation}
       style={tw`flex-row items-center px-4 py-3`}>
       <Image
-        source={picture ? {uri: picture} : imageByGender}
+        source={
+          picture
+            ? {
+                uri: `${
+                  __DEV__ ? config.REACT_APP_DEV_URI : config.REACT_APP_PROD_URI
+                }/${picture}`,
+              }
+            : imageByGender
+        }
         resizeMode="contain"
         style={tw.style('rounded-full', {
           width: 40,
@@ -33,13 +45,44 @@ const FollowItem = ({
         })}
       />
       <View style={tw`flex-1 px-4`}>
-        <Text style={tw`font-avSemi text-sm text-black`}>{fullName}</Text>
-        <Text style={tw`font-avReg text-xs text-black`}>
-          <Text style={tw`font-avSemi text-blue-500`}>{activities}</Text>{' '}
-          Activities |{' '}
-          <Text style={tw`font-avSemi text-blue-500`}>{followers}</Text>{' '}
-          Followers
-        </Text>
+        <View style={tw`flex-row items-center`}>
+          <Text style={tw`mr-1 font-avSemi text-sm text-black`}>
+            {fullName}
+          </Text>
+          {isAdmin && (gender === 'MALE' || gender === 'FEMALE') && (
+            <MaterialCommunityIcons
+              name={gender === 'MALE' ? 'gender-male' : 'gender-female'}
+              size={16}
+              color={
+                gender === 'MALE' ? tw.color('blue-600') : tw.color('pink-600')
+              }
+            />
+          )}
+        </View>
+        <View style={tw`flex-row items-center`}>
+          <Text style={tw`font-avReg text-xs text-black`}>
+            <Text style={tw`font-avSemi text-blue-500`}>{activities}</Text>{' '}
+            Activities |{' '}
+            <Text style={tw`font-avSemi text-blue-500`}>{followers}</Text>{' '}
+            Followers
+          </Text>
+          {isAdmin && (
+            <>
+              <Text
+                style={tw`capitalize mx-1 px-2 py-[2px] font-avReg text-[10px] text-center text-white bg-${
+                  rest?.platform === 'android' ? 'cyan-600' : 'violet-600'
+                } rounded`}>
+                {rest?.platform}
+              </Text>
+              {rest?.role !== 'USER' && (
+                <Text
+                  style={tw`capitalize mx-1 px-2 py-[2px] font-avReg text-[10px] text-center text-white bg-blue-600 rounded`}>
+                  {rest?.role}
+                </Text>
+              )}
+            </>
+          )}
+        </View>
       </View>
     </Pressable>
   );
--- screens/AdminScreens/AdminUserListScreen.js
+++ screens/AdminScreens/AdminUserListScreen.js
@@ -1,11 +1,92 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import tw from '@lib/tailwind';
+import React, {useCallback} from 'react';
+import {gql, useQuery} from '@apollo/client';
+import {showMessage} from 'react-native-flash-message';
+import {FollowItem, SafeAreaContainer} from '@components';
+import {View, FlatList, Text, RefreshControl} from 'react-native';
+
+const USERS = gql`
+  query users(
+    $offset: Int
+    $limit: Int
+    $search: String
+    $filter: UsersFilterInput
+  ) {
+    users(offset: $offset, limit: $limit, search: $search, filter: $filter) {
+      code
+      success
+      message
+      token
+      limit
+      offset
+      payload {
+        __typename
+        _id
+        fullName
+        gender
+        picture
+        userVerified
+        role
+        platform
+        createdAt
+        updatedAt
+        followers
+        activities
+      }
+    }
+  }
+`;
 
 const AdminUserListScreen = () => {
+  const {loading, data, refetch, fetchMore} = useQuery(USERS, {
+    variables: {},
+    onError: err => {
+      showMessage({
+        message: err?.message || 'Some unknown error occurred. Try again!!',
+        type: 'danger',
+      });
+    },
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const _renderItem = useCallback(
+    ({item}) => <FollowItem {...item} isAdmin={true} />,
+    [],
+  );
+
   return (
-    <View>
-      <Text>AdminUserListScreen</Text>
-    </View>
+    <SafeAreaContainer
+      statusBgColor={tw.color('blue-600')}
+      statusBarStyle="light-content">
+      <FlatList
+        bounces={true}
+        //
+        data={data?.users?.payload}
+        keyExtractor={item => item._id}
+        renderItem={_renderItem}
+        //
+        contentContainerStyle={tw`py-2`}
+        ListFooterComponent={() => (
+          <View style={tw`py-4 items-center`}>
+            <Text style={tw`text-xs text-black font-avSemi`}>
+              {loading ? 'Loading...' : "That's all, we have got!!"}
+            </Text>
+          </View>
+        )}
+        //
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={refetch} />
+        }
+        onEndReached={() => {
+          fetchMore({
+            variables: {
+              offset: data?.users?.payload.length,
+              limit: 10,
+            },
+          });
+        }}
+      />
+    </SafeAreaContainer>
   );
 };
 
